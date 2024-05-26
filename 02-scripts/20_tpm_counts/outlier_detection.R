0a9364d4e1646160cf6cc58b376a772a9e38a0dd
Erika Kvalem <erika.kvalem@student.i-med.ac.at>
2024-05-26 16:09:19 +0200

    Find outliers

--- 02-scripts/20_tpm_counts/outlier_detection.R
+++ 02-scripts/20_tpm_counts/outlier_detection.R
@@ -2,15 +2,86 @@ library(bigutilsr)
 library(ggplot2)
 #theme_set(bigstatsr::theme_bigstatsr(0.8))
 
-path ="/data/projects/2024/duszka/NB_project/data/results_counts_raw_tpm_ann/"
+## INTESTINE
+counts = read.csv("/data/scratch/kvalem/projects/2024/duszka_2024/02-scripts/tables/20_tpm_counts/counts_matrix_metadata_intestine.csv")
+counts <- counts[, -(1:2)]
+# List of column names to remove
+columns_to_remove <- c('SampleID', 'mouseID', 'group', 'subgroup', 'source', 'sequencingID','sample_type')
 
-file ="Galaxy3603-Column_Join_count_data_tpm.tsv"
+# Remove columns from the dataframe
+X <- counts[, !(names(counts) %in% columns_to_remove)]
 
-counts_tpm <- read.csv(paste0(path,file), sep = "\t")
+#PCA
+pca <- prcomp(X, retx = TRUE,  scale = TRUE,tol=0.4)
+predicted <-predict(pca,X)
 
-pca <- prcomp(counts_tpm, scale. = TRUE, rank. = 10)
-U <- pca$x
+predicted <- cbind(predicted, sequencingID = counts$sequencingID)
+ggplot(data.frame(predicted))+aes(x=PC1,y=PC2,color=counts$sample_type)+geom_point(aes(size=0.5))+stat_ellipse()+stat_ellipse(level=0.8)
 
-qplot(U[, 1], U[, 2]) + coord_equal()
 
-df_out <- apply(U, 2, function(x) which( abs(x - mean(x)) > (6 * sd(x)) ))
+merged_data <- merge(predicted, counts, by = "sequencingID")
+#PLOT PCA
+ggplot(merged_data) +
+  aes(x = PC1, y = PC2, color = sample_type) +
+  geom_point(size = 0.5) +
+  stat_ellipse() +
+  stat_ellipse(level = 0.8) +
+  geom_text(aes(label = sequencingID), hjust = 0, vjust = 0)
+
+# Calculate outliers
+data_transposed <- t(X)
+pca_result <- prcomp(data_transposed, scale. = TRUE)
+
+#
+## Calculate distances from centroid
+centroid <- colMeans(pca_result$x)
+distances <- apply(pca_result$x, 2, function(x) sqrt(sum((x - centroid)^2)))
+
+# Determine outliers based on threshold (more than 3 standard deviations)
+outlier_threshold <- mean(distances) + 3 * sd(distances)
+outliers <- which(distances > outlier_threshold)
+
+# Print names of outlier samples
+print(names(data_transposed[outliers, ]))
+
+## LIVER
+counts = read.csv("/data/scratch/kvalem/projects/2024/duszka_2024/02-scripts/tables/20_tpm_counts/counts_matrix_metadata_liver.csv")
+counts <- counts[, -(1:2)]
+# List of column names to remove
+columns_to_remove <- c('SampleID', 'mouseID', 'group', 'subgroup', 'source', 'sequencingID','sample_type')
+
+# Remove columns from the dataframe
+X <- counts[, !(names(counts) %in% columns_to_remove)]
+
+#PCA
+pca <- prcomp(X, retx = TRUE,  scale = TRUE,tol=0.4)
+predicted <-predict(pca,X)
+
+predicted <- cbind(predicted, sequencingID = counts$sequencingID)
+ggplot(data.frame(predicted))+aes(x=PC1,y=PC2,color=counts$sample_type)+geom_point(aes(size=0.5))+stat_ellipse()+stat_ellipse(level=0.8)
+
+
+merged_data <- merge(predicted, counts, by = "sequencingID")
+#PLOT PCA
+ggplot(merged_data) +
+  aes(x = PC1, y = PC2, color = sample_type) +
+  geom_point(size = 0.5) +
+  stat_ellipse() +
+  stat_ellipse(level = 0.8) +
+  geom_text(aes(label = sequencingID), hjust = 0, vjust = 0)
+
+# Calculate outliers
+data_transposed <- t(X)
+pca_result <- prcomp(data_transposed, scale. = TRUE)
+
+#
+## Calculate distances from centroid
+centroid <- colMeans(pca_result$x)
+distances <- apply(pca_result$x, 2, function(x) sqrt(sum((x - centroid)^2)))
+
+# Determine outliers based on threshold (more than 3 standard deviations)
+outlier_threshold <- mean(distances) + 3 * sd(distances)
+outliers <- which(distances > outlier_threshold)
+
+# Print names of outlier samples
+print(names(data_transposed[outliers, ]))
